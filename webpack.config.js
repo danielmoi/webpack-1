52a135bef6592a5067ff625fd23cd3c624db1d39
danielmoi <daniel@ampersandmoi.com>
2016-06-06 09:44:54 +1000

    Use JSHint pre-loader with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,18 @@ module.exports = {
   },
   watch: true,
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'jshint-loader'
+
+    }],
     loaders: [{
       test: /\.es6$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
+        cacheDirectory: true,
         presets: ['react', 'es2015']
       }
     }]
